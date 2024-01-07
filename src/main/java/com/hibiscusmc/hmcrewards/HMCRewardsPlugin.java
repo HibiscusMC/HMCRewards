8059b935e36ac70fdafc9f96683c02dd5483cdad
yusshu <yushu@unnamed.team>
2024-01-07 01:40:40 -0500

    *: extend HibiscusPlugin, implement Module

--- src/main/java/com/hibiscusmc/hmcrewards/HMCRewardsPlugin.java
+++ src/main/java/com/hibiscusmc/hmcrewards/HMCRewardsPlugin.java
@@ -1,17 +1,33 @@
 package com.hibiscusmc.hmcrewards;
 
+import com.hibiscusmc.hmcrewards.feedback.FeedbackModule;
 import com.hibiscusmc.hmcrewards.util.ConfigurationManager;
+import me.lojosho.hibiscuscommons.HibiscusPlugin;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import team.unnamed.inject.Binder;
+import team.unnamed.inject.Module;
 
 @SuppressWarnings("unused")
-public final class HMCRewardsPlugin extends JavaPlugin {
+public final class HMCRewardsPlugin extends HibiscusPlugin implements Module {
     public static final String GLOBAL_CONFIG = "config.yml";
     public static final String MENU_CONFIG = "menu.yml";
 
     @Override
-    public void onEnable() {
+    public void onStart() {
         final var configurationManager = ConfigurationManager.create(this);
         configurationManager.getOrCreate(GLOBAL_CONFIG);
         configurationManager.getOrCreate(MENU_CONFIG);
+    }
+
+    @Override
+    public void configure(Binder binder) {
+        // bind plugin!
+        binder.bind(HMCRewardsPlugin.class).toInstance(this);
+        binder.bind(JavaPlugin.class).to(HMCRewardsPlugin.class);
+        binder.bind(Plugin.class).to(HMCRewardsPlugin.class);
+
+        // install modules
+        binder.install(new FeedbackModule());
     }
 }
