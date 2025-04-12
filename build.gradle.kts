2f6a0bfcb990e2b5245ab473e9f2547b096e3ebc
Kirzu <65211478+NotKirzu@users.noreply.github.com>
2025-04-12 14:43:23 -0500

    chore: bump up patch version (0.5.2 -> 0.5.3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ tasks {
     runServer {
         downloadPlugins {
             github("dmulloy2", "ProtocolLib", "5.3.0", "ProtocolLib.jar")
-            url("https://repo.hibiscusmc.com/releases/me/lojosho/HibiscusCommons/0.6.0/HibiscusCommons-0.6.0-all.jar")
+            url("https://repo.hibiscusmc.com/releases/me/lojosho/HibiscusCommons/0.6.3/HibiscusCommons-0.6.3-all.jar")
         }
         minecraftVersion("1.21.1")
     }
