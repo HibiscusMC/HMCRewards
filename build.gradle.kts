62ef709b75a4356cf35715ba9e5bef7ff706ca04
LoJoSho <lojoshogaming@gmail.com>
2025-06-28 12:05:23 -0500

    chore: update gradle (and plugins)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("xyz.jpenilla.run-paper") version "2.3.0"
-    id("io.github.goooler.shadow") version "8.1.7"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("com.gradleup.shadow") version "8.3.2"
     id("maven-publish")
 }
 
